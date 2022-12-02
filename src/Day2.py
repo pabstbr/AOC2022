96d534e8c445ead9cb79500b131d3ead6fef04bb
Patrick Roberts <patrickbaylor@gmail.com>
2022-12-02 10:13:52 -0500

    Day2 minor legibility refactor.

--- src/Day2.py
+++ src/Day2.py
@@ -3,7 +3,7 @@ import csv
 import string
 
 moves = list()
-score = 0
+part1Score = 0
 part2Score = 0
 
 with open('../resources/Day2Input', 'r') as inputFile:
@@ -11,23 +11,25 @@ with open('../resources/Day2Input', 'r') as inputFile:
     for row in strategyReader:
         currScore = 0
         opponent = string.ascii_uppercase.index(row[0]) + 1
-        outcome = string.ascii_uppercase.index(row[1]) - 22
-        score += outcome
+        player = string.ascii_uppercase.index(row[1]) - 22
 
-        if outcome == 1:
+        # Part1
+        part1Score += player
+        if opponent == player:
+            part1Score += 3
+        elif (opponent - player) % 3 == 2:
+            part1Score += 6
+
+        # Part2
+        if player == 1:
             part2Score += 0
             part2Score += (opponent + 2) % 3 if opponent + 2 > 3 else opponent + 2
-        elif outcome == 2:
+        elif player == 2:
             part2Score += 3
             part2Score += opponent
-        elif outcome == 3:
+        elif player == 3:
             part2Score += 6
             part2Score += (opponent + 1) % 3 if opponent + 1 > 3 else opponent + 1
 
-        if opponent == outcome:
-            score += 3
-        elif opponent - outcome == -1 or opponent - outcome == 2:
-            score += 6
-
-print(f'Part1: {score}')
+print(f'Part1: {part1Score}')
 print(f'Part2: {part2Score}')
