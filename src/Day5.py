21b5e1b0325225b2ed6be21e38dda0cb7cb16144
Patrick Roberts <patrickbaylor@gmail.com>
2022-12-05 07:53:47 -0500

    Day 5 solution. I, honestly, dislike the enumerate solution for parsing the data, but it works so...

---
+++ src/Day5.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+import copy
+import re
+from collections import deque
+
+crateState, moves = open('../resources/Day5input', 'r').read().split('\n\n')
+stacks = []
+
+for line in crateState.splitlines()[:-1]:
+    print(line)
+    for stackNum, crateValPos in enumerate(range(1, len(line), 4)):
+        while stackNum >= len(stacks):
+            stacks.append(deque())
+        if line[crateValPos] != ' ':
+            stacks[stackNum].append(line[crateValPos])
+
+# I hate references a bit right now
+stacksDupe = copy.deepcopy(stacks)
+
+for move in moves.splitlines():
+    numMoved, fromStack, toStack = map(int, re.findall(r"\d+", move))
+
+    # Part 1
+    for crate in range(numMoved):
+        stacks[toStack - 1].appendleft(stacks[fromStack - 1].popleft())
+
+    # Part 2
+    craneHolding = deque()
+    print(stacksDupe)
+    for crate in range(numMoved):
+        craneHolding.appendleft(stacksDupe[fromStack - 1].popleft())
+    stacksDupe[toStack - 1].extendleft(craneHolding)
+
+print(f'Part 1: {"".join(x[0] for x in stacks)}')
+print(f'Part 2: {"".join(x[0] for x in stacksDupe)}')
