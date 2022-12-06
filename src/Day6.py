5ddf7c4de795d4f212574785aa7a4bd446a58900
Patrick Roberts <patrickbaylor@gmail.com>
2022-12-06 08:43:33 -0500

    Day 6 solutions. Sets Uber Alles

---
+++ src/Day6.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+signal = open('../resources/Day6input', 'r').read()
+
+# Part 1
+firstMarker = 0
+for marker in enumerate(range(1, len(signal))):
+    print(signal[marker[1]-4:marker[1]])
+    if len(set(signal[marker[1]-4:marker[1]])) == 4:
+        firstMarker = marker[1]
+        break
+
+print(f'First Marker: {firstMarker}')
+
+# Part 2
+longMarker = 0
+for marker in enumerate(range(1, len(signal))):
+    print(signal[marker[1]-14:marker[1]])
+    if len(set(signal[marker[1]-14:marker[1]])) == 14:
+        longMarker = marker[1]
+        break
+
+print(f'Long Marker: {longMarker}')
